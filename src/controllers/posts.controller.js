d5cad8e347b3f69ecf5c183fe955faedc6336720
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-06-02 18:05:55 -0500

    [+] View > Posts: Show the data obtained from the API in the view

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -7,16 +7,39 @@ const Posts = async () => {
     el .classList = 'page page-posts';   // Agrega clases al elemento principal de la vista
     el .innerHTML = view;               // Anida el contenido del String dentro del nuevo elemento 'main' 
 
+    let $totalEl = el .querySelector( '#total' );
+
     /** Obtenemos los datos del API */
     const posts = await getPosts();
+    $totalEl .innerHTML = posts .length;
+    
     console .log( 'Posts', posts );     // Despliega en la consola del navegador
+
+    /** Obtenemos el elemento de la vista donde queremos agregar las publicaciones */
+    const $postsEl = el .querySelector( '#posts' );
+
+    posts .forEach( post => {
+        const template = `
+            <a href="#" class="list-group-item list-group-item-action">
+                <div class="d-flex w-100 justify-content-between">
+                    <h5 class="mb-1">${ post .title }</h5>
+                    <small>${ post .id }</small>
+                </div>
+                <p class="mb-1">${ post .body }</p>
+                <small>Donec id elit non mi porta.</small>
+            </a>
+        `;
+
+        $postsEl .innerHTML += template;
+    });
+
 
     return el;
 }
 
 /** Realiza petición al API */
 const getPosts = async () => {
-    const response = await fetch( `https://jsonplaceholder.typicode.com/posts` );
+    const response = await fetch( `https://jsonplaceholder.typicode.com/posts?_limit=10` );
         
     return await response .json();
 }
