156760e4111f9293980b389660190baacb7ea10d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-06-02 11:04:21 -0500

    [+] NPM > html-webpack-plugin: Implement and defines basic preconfiguration

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
+const HTMLWebPackPlugin =  require( 'html-webpack-plugin' );
+
 module .exports = {
     entry: './src/master.js',
     output:  {
         path: __dirname + '/dist',
         filename: 'bundle.js'
-    }
+    },
+    plugins: [
+        new HTMLWebPackPlugin()
+    ]
 }
